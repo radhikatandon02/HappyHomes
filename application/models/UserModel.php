af3610706c159c93e7a26d5d700dc277b0cb4b28
radzz <92901909+radhikatandon02@users.noreply.github.com>
2022-05-29 15:36:20 +0530

    2nd Commit

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -1,7 +1,7 @@
-  <!--
-	Model 
--->
-
+ <!--
+	Model   
+-->   
+ 
 <?php 
 	
 	class UserModel extends CI_model{
@@ -10,9 +10,19 @@
 
 			//print_r($data);
 
-			$result=$this->db->where("password" , $data['password'])->where("username" , $data['username'])->where("email" , $data['email'])->get('userLogin')->row_array();
+			$result=$this->db->where("password" , $data['password'])->where("username" , $data['username'])->where("email" , $data['email'])->get('userlogin')->row_array();
 			
 			return $result;
+		}
+
+		public function create($data){
+			$this->db->insert("userlogin", $data);
+		}
+
+		public function update($data){
+			$id=$this->session->userdata("users")['id'];
+			$this->db->where("id",$id)->update("userlogin", $data);
+			//return;
 		}
 
 		public function validateAdminData($data){
@@ -23,5 +33,6 @@
 			
 			return $result;
 		}
+
 	}
 ?>
